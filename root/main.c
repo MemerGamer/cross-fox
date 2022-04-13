9b47e19005191fdb679beb453ca0b4126af57fc4
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-04-14 00:16:11 +0300

    implementing the demo

--- root/main.c
+++ root/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+#include "../demo/demo.h"
 int main() {
-    printf("Hello, Cross-Fox\n");
+    printf("Hello, Cross-Fox App\n");
+    //test demo
+    demo();
     return 0;
 }
