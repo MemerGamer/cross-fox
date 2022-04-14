b96ddf91521cecaced9032a6bdd9575d2141d70b
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-04-14 15:35:26 +0300

    creating server struct

--- fox/headers/models/networking/server.h
+++ fox/headers/models/networking/server.h
@@ -1,8 +1,30 @@
 //
-// Created by kovac on 2022. 04. 14..
+// Created by kovacs on 2022. 04. 14..
 //
 
 #ifndef CROSS_FOX_SERVER_H
 #define CROSS_FOX_SERVER_H
+
+#include <WinSock2.h>
+
+//for Unix systems
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+
+typedef struct{
+    int domain;
+    int service;
+    int protocol;
+    u_long _interface;
+    int port;
+    int backlog;
+
+    struct sockaddr_in address;
+
+    void (*launch)(void);
+}FoxServer;
+
+struct FoxServer fox_server_constructor(int domain, int service, int protocol, u_long _interface, int port, int backlog,void(*launch)(void));
+
 
 #endif //CROSS_FOX_SERVER_H
