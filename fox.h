ec0188e4e20efb94dd7b57c28e4315c57a0c5f2d
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-04-14 15:09:03 +0300

    creating an universal header file

---
+++ fox.h
@@ -0,0 +1,10 @@
+//
+// Created by kovac on 2022. 04. 14..
+//
+
+#ifndef CROSS_FOX_FOX_H
+#define CROSS_FOX_FOX_H
+
+//this header file will contain all the headers needed for using the API
+
+#endif //CROSS_FOX_FOX_H
